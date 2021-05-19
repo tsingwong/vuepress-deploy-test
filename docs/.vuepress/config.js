2f845f3aeea0fe05a0d07a73af9dbeef64b066fa
tsingwong <tsingwoong@163.com>
2021-05-19 11:23:31 +0800

    feat: init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+/*
+ * @Description:
+ * @Author: Tsingwong
+ * @Date: 2021-05-19 11:12:50
+ * @LastEditors: Tsingwong
+ * @LastEditTime: 2021-05-19 11:13:46
+ */
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  base: '/vuepress-deploy-test/',
+}
